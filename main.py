2660732233187ad959a1c910742a9dd11c9e935e
Nathan Contino <WinterMute@Nathans-MacBook-Pro.local>
2016-07-24 15:13:32 -0400

    now scrapes all /wiki/ links from a given wikipedia page.

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import urllib.request
+import re
 
 def main():
-	local_filename, headers = urllib.request.urlretrieve('http://python.org/')
+	local_filename, headers = urllib.request.urlretrieve('https://en.wikipedia.org/wiki/Ubuntu_(operating_system)')
 	html = open(local_filename)
-	print(findEveryURLOnPage(html.read()))
+	goodURLS = filterUnpleasantURLs(findEveryURLOnPage(html.read()))
+	print(','.join(goodURLS));
+
+def filterUnpleasantURLs(urls):
+	genericWikiPageStart = '/wiki/'
+	return [link for link in urls if link.startswith(genericWikiPageStart)];
 
 
 def findEveryURLOnPage(page):
 	#first, we want to filter our page down to just the content
 	pageContent = findWikipediaPageBody(page);
-	return pageContent; #currently just return page content, no url finding
-
+	urls = re.findall(r'href=[\'"]?([^\'" >]+)', pageContent)
+	return urls;
 
 def findWikipediaPageBody(page):
 	contentStartTag = "<div id=\"mw-content-text\" lang=\"en\" dir=\"ltr\" class=\"mw-content-ltr\">";
