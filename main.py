2478cfb66c3eb82a8ad0c6f1fd321b400214d42d
Nathan Contino <WinterMute@Nathans-MacBook-Pro.local>
2016-07-24 14:39:22 -0400

    basic page filtering
    Probably moderately wrong.

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ local_filename, headers = urllib.request.urlretrieve('http://python.org/')
 html = open(local_filename)
 print(html.read())
 
+
+def findEveryURLOnPage(page):
+	//first, we want to filter our page down to just the content
+	pageContent = findWikipediaPageBody(page);
+	return pageContent; //currently just return page content, no url finding
+
+
+def findWikipediaPageBody(page):
+	contentStartTag = "<div id=\"mw-content-text\" lang=\"en\" dir=\"ltr\" class=\"mw-content-ltr\">";
+	return page.partition(contentStartTag)[2]; 
