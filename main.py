cbcdaa53f3a96be5199b950d2179d988dbbd24fc
Nathan Contino <WinterMute@Nathans-MacBook-Pro.local>
2016-07-24 14:42:25 -0400

    minor refactor
    now uses actual python comments (seriously why can't we be civilised and use '//'?), a beautiful main function, and actually runs. Too bad there's no content on the main wikipedia page to filter...

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import urllib.request
-local_filename, headers = urllib.request.urlretrieve('http://python.org/')
-html = open(local_filename)
-print(html.read())
+
+def main():
+	local_filename, headers = urllib.request.urlretrieve('http://python.org/')
+	html = open(local_filename)
+	print(findEveryURLOnPage(html.read()))
 
 
 def findEveryURLOnPage(page):
-	//first, we want to filter our page down to just the content
+	#first, we want to filter our page down to just the content
 	pageContent = findWikipediaPageBody(page);
-	return pageContent; //currently just return page content, no url finding
+	return pageContent; #currently just return page content, no url finding
 
 
 def findWikipediaPageBody(page):
 	contentStartTag = "<div id=\"mw-content-text\" lang=\"en\" dir=\"ltr\" class=\"mw-content-ltr\">";
-	return page.partition(contentStartTag)[2]; 
+	return page.partition(contentStartTag)[2];
+
+main(); 
