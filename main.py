5df3c71f7ccdea03efa1fbd9a146c52107b19a0e
Nathan Contino <WinterMute@android-6dc350463ab65601.home>
2016-07-24 23:19:36 -0400

    Pages objects, so everything isn't such a fustercluck

--- main.py
+++ main.py
@@ -2,31 +2,61 @@ import urllib.request
 import re
 import hashlib
 
-visitedURLs = {};
+pagesCollection = {};
+
+class Page(object):
+	connections = [];
+
+	def __init__(self, title):
+		self.title = title;
+
+	def print_connections(self):
+		for c in connections:
+			print(c + '\n');
+	
+	def url(self):
+		return 'https://en.wikipedia.org/wiki/' + self.title;
+	
 
 def main():
-	startPage = '/wiki/Ubuntu_(operating_system)'
-	linksToVisit = [];
-	linksToVisit.append(startPage);
-	while linksToVisit:
-		linksToVisit.extend(fetchPageLinks(linksToVisit.pop(0)))
+	startPage = Page('Ubuntu_(operating_system)');
+	pagesToVisit = [];
+	pagesToVisit.append(startPage);
+	while pagesToVisit:
+		pagesToVisit.extend(fetchPageLinks(pagesToVisit.pop(0)))
 	print('donezo');
 
-def fetchPageLinks(url):
-	wikipediaURL = 'https://en.wikipedia.org' + (url);
+def fetchPageLinks(page):
+	wikipediaURL = page.url();
 	local_filename, headers = urllib.request.urlretrieve(wikipediaURL)
 	html = open(local_filename)
 	goodURLs = filterUnpleasantURLs(findEveryURLOnPage(html.read()))
-	unvisitedGoodURLs = removeRepeatURLs(goodURLs);
-	print(',\n'.join(unvisitedGoodURLs));
-	return unvisitedGoodURLs;
+	
+	pageLinks = pageNamesFromURLs(goodURLs);	
+	unvisitedGoodPages = removeRepeatURLs(pageLinks);
+	
+	#now that a Page exists in our Page collection for all of these URLs, link our current Page to each one
+	for currPageLink in pageLinks:
+		page.connections.append(pagesCollection[currPageLink]);
 
-def removeRepeatURLs(urls):
+	pagesToVisit = [];
+	for g in unvisitedGoodPages:
+		pagesToVisit.append(pagesCollection[g]);
+	
+	print(',\n'.join(unvisitedGoodPages));
+	return pagesToVisit;
+
+def pageNamesFromURLs(urls):
+	pageNames = [];
+	for u in urls:
+		pageNames.append(u.split('/wiki/')[1]);
+	return pageNames;
+
+def removeRepeatURLs(pageLinks):
 	unvisitedURLs = [];
-	for link in urls:
-		page_name = link.split('/wiki/')[1]
-		if not visitedURLs.get(page_name, False):
-			visitedURLs[page_name] = True;
+	for link in pageLinks:
+		if not pagesCollection.get(link, False):
+			pagesCollection[link] = Page(link);
 			unvisitedURLs.append(link);
 	return unvisitedURLs;
 	
@@ -39,6 +69,7 @@ def filterUnpleasantURLs(urls):
 def findEveryURLOnPage(page):
 	#first, we want to filter our page down to just the content
 	pageContent = findWikipediaPageBody(page);
+	#then use this handy regex to filter things down to just the good stuff (that is, valid wiki pages in english)
 	urls = re.findall(r'href=[\'"]?([^\'" >]+)', pageContent)
 	return urls;
 
